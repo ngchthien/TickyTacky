63496225c8d3ebe404b55bed207c4baa187eb45f
ngchthien <thienngo.tech@gmail.com>
2026-04-07 13:13:59 +0700

    feat: implement app mode state management using Factory dependency injection

---
+++ TickyTacky/Core/AppModelViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AppModelViewModel.swift
+//  TickyTacky
+//
+//  Created by M1 Pro on 7/4/26.
+//
+
+import Foundation
---
+++ TickyTacky/Extensions/Container+Registration.swift
@@ -0,0 +1,8 @@
+//
+//  Container+Registration.swift
+//  TickyTacky
+//
+//  Created by M1 Pro on 7/4/26.
+//
+
+import Foundation
