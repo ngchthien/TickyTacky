7398c6d3f7e459cb3642ffa138f4efcb20558e06
ngchthien <thienngo.tech@gmail.com>
2026-04-11 20:19:48 +0700

    feat: implement game state domain models and result view UI

---
+++ TickyTacky/Core/GameResultView.swift
@@ -0,0 +1,18 @@
+//
+//  GameResultView.swift
+//  TickyTacky
+//
+//  Created by M1 Pro on 11/4/26.
+//
+
+import SwiftUI
+
+struct GameResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameResultView()
+}
