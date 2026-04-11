51e4e9bb35e4fd1619df76535431823cfaddecfd
ngchthien <thienngo.tech@gmail.com>
2026-04-11 20:51:24 +0700

    feat: implement AnalyticsService and ErrorHandlerService with supporting domain models and dependency injection

---
+++ TickyTacky/Domain/GameError.swift
@@ -0,0 +1,8 @@
+//
+//  GameError.swift
+//  TickyTacky
+//
+//  Created by M1 Pro on 11/4/26.
+//
+
+import Foundation
---
+++ TickyTacky/Domain/GameResult.swift
@@ -0,0 +1,8 @@
+//
+//  GameResult.swift
+//  TickyTacky
+//
+//  Created by M1 Pro on 11/4/26.
+//
+
+import Foundation
